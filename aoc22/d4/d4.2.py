8e30264a96a53c267e656841b4f85103fb00a267
Andre <AndreAndreM@proton.me>
2023-08-28 01:13:41 +0200

    day4.2

---
+++ aoc22/d4/d4.2.py
@@ -0,0 +1,24 @@
+
+with open("data4.txt") as f:
+    counter = 0
+
+    for line in f:
+        first_first = int(line[0:line.find("-")])
+        first_second = int(line[line.find("-")+1:line.find(",")])
+
+        second_first = int(line[line.find(",")+1:line.find("-", line.find(","))])
+        second_second = int(line[line.find("-", line.find(","))+1:line.find("\n")])
+
+        added = False
+
+        if first_first <= second_first:
+            if first_second >= second_first:
+                counter += 1
+                added = True
+
+        if second_first <= first_first and not added:
+            if second_second >= first_first:
+                counter += 1
+
+
+print(f"Part 2: {counter}")

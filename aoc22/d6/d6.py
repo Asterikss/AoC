79da31e8b008baabbd2c768d60a45679507982f8
Andre <AndreAndreM@proton.me>
2023-08-31 18:08:32 +0200

    day6

---
+++ aoc22/d6/d6.py
@@ -0,0 +1,9 @@
+
+with open("data6.txt") as f:
+    input = f.read()
+
+    for i in range(len(input) - 4):
+        if len(set(input[i:i+4])) == len(input[i:i+4]):
+            print(input[i:i+4])
+            print(f"Part 1: {i + 4}")
+            break

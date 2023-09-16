d8b24c6b27d7c49fc40a0d41119d3cd51c8b0eb6
Andre <AndreAndreM@proton.me>
2023-09-16 19:14:11 +0200

    d10

---
+++ aoc22/day10/d10.py
@@ -0,0 +1,26 @@
+cycle = 0
+cycle_table = [20 + 40 * x for x in range(6)]
+cycle_values = []
+x = 1
+
+with open("data10.txt", "r") as f:
+    for line in f:
+        value = 0
+        if line.find("noop") == -1:
+            command, value = line.split(" ")
+        else:
+            command = "noop"
+        cycle += 1
+
+        if cycle in cycle_table:
+            cycle_values.append(x * cycle)
+
+        if command != "noop":
+            cycle += 1
+
+            if cycle in cycle_table:
+                cycle_values.append(x * cycle)
+            x += int(value)
+
+
+print(f"Part 1: {sum(cycle_values)}")
